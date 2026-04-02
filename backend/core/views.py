6acf030c55adddb6791c3a9dce2a3a2064849ca9
josesegundo <jose.ardinoseg@gmail.com>
2026-04-02 11:10:39 -0300

    Implementação do modelo Tarefa com serializer e endpoints da API

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tarefa
+from .serializers import TarefaSerializer
 
-# Create your views here.
+class TarefaViewSet(viewsets.ModelViewSet):
+    queryset = Tarefa.objects.all().order_by('-criada_em')
+    serializer_class = TarefaSerializer
