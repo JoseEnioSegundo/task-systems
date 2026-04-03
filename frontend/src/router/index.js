981bb1c0a10de0583769f2dc53483654ec302064
josesegundo <jose.ardinoseg@gmail.com>
2026-04-03 14:04:36 -0300

    refactor: simplificar configuração do roteador e remover código redundante

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,32 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import TarefasView from '../views/TarefasView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import TarefasView from "@/views/TarefasView.vue";
+import AboutView from "@/views/AboutView.vue";
 
-
+const routes = [
+  { path: "/", name: "home", component: HomeView },
+  { path: "/tarefas", name: "tarefas", component: TarefasView },
+  { path: "/about", name: "about", component: AboutView },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+  history: createWebHistory(),
+  routes,
+});
 
-    },
-    {
-      path: '/',
-      name: 'tarefas',
-      component: TarefasView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
-
-export default router
+export default router;
