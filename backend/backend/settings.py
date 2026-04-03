9cc7adefef00327041a5d734e78b4a3f36ad4537
josesegundo <jose.ardinoseg@gmail.com>
2026-04-03 14:03:57 -0300

    feat: adicionar suporte a CORS e atualizar middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "rest_framework",
+    "corsheaders",
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [    
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -117,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+CORS_ALLOW_ALL_ORIGINS = True
