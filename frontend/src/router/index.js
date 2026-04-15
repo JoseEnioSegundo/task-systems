711fafe0c39f4cfbfa0f782dae238371647e80bf
josesegundo <jose.ardinoseg@gmail.com>
2026-04-15 08:54:52 -0300

    feat: adicionar rota de login ao roteador

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import TarefasView from "@/views/TarefasView.vue";
 import AboutView from "@/views/AboutView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/tarefas", name: "tarefas", component: TarefasView },
   { path: "/about", name: "about", component: AboutView },
+  { path: "/login", component: LoginView },
 ];
 
 const router = createRouter({
